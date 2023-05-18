25d330ebd02992b1e90d24eb67d3a9d40928f2a5
j <j>
2023-05-18 22:17:56 +0100

    initial commit

---
+++ TachoClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using PCSC;
+
+namespace TachoClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start");
+            try
+            {
+                ISCardContext Context = ContextFactory.Instance.Establish(SCardScope.System);
+                var readers = Context.GetReaders();
+                Console.WriteLine("Found {0} reader(s)!", readers.Length);
+                foreach(var reader in readers)
+                {
+                    Console.WriteLine(reader);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            Console.WriteLine("End");
+        }
+    }
+}
