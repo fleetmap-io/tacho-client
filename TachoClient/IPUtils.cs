44e68f52bba684d5f493ba9bd26dda03e9735ce8
j <j>
2024-01-18 14:07:50 +0000

    Get Tacho IP from alblambda

---
+++ TachoClient/IPUtils.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+
+namespace TMService
+{
+    public static class IPUtils
+    {
+        public static string GetTachoServerIp()
+        {
+            var ip = new HttpClient().GetAsync("https://api.pinme.io/alblambda/ipsolver?service=TachoServer")
+                .GetAwaiter().GetResult()
+                .Content.ReadAsStringAsync().GetAwaiter().GetResult().Trim();
+
+            return IPAddress.Parse(ip).ToString();
+        }
+    }
+}
--- TachoClient/Settings.cs
+++ TachoClient/Settings.cs
@@ -1,11 +1,13 @@
-﻿namespace TachoClient
+﻿using TMService;
+
+namespace TachoClient
 {
     public class Settings
     {
         private Settings() { }
         public static Settings Default = new Settings();
 
-        public string ServerIP = "gps.pinme.io";
+        public string ServerIP => IPUtils.GetTachoServerIp();
         public int ServerPort = 9096;
         public int ConnectionFirstTimeOutMinutes = 5;
         public int ConnectionTimeOutMinutes = 5;
