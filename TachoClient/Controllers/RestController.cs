985d1d743bb9b0589adc1f4654774f967ec7d930
j <j>
2024-12-17 02:00:27 +0000

    rest controller

---
+++ TachoClient/Controllers/RestController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TachoClient.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class RestController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("ok");
+        }
+
+        [HttpPost]
+        public IActionResult Send(Device device, string apdu)
+        {
+            //var companyId = device.attributes.clientId;
+            return Ok("9000");
+        }
+
+        public class Device
+        {
+            public DeviceAttributes attributes { get; set; }
+        }
+
+        public class DeviceAttributes
+        {
+            public int clientId { get; set; }
+        }
+    }
+}
