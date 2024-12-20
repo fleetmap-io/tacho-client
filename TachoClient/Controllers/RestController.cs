1537a56b2aea76a8c04ada3b04108123bb2ecba7
jcardus <asklocation.net@gmail.com>
2024-12-20 01:04:33 +0000

    Update RestController.cs

--- TachoClient/Controllers/RestController.cs
+++ TachoClient/Controllers/RestController.cs
@@ -177,7 +177,7 @@ namespace TachoClient.Controllers
         }
 
         [HttpGet("/version")]
-        public IActionResult Icc()
+        public IActionResult Version()
         {
             return Ok("1.0");
         }
