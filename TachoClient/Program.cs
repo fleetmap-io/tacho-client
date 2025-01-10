294b48d8a5aad0a2d69046f1b443efbafff5d043
j <j>
2025-01-10 13:47:00 +0000

    only rest again take 3

--- TachoClient/Program.cs
+++ TachoClient/Program.cs
@@ -82,7 +82,9 @@ namespace TachoClient
             try
             {
                 context = ContextFactory.Instance.Establish(SCardScope.System);
-                // SendReadersInfo(context);
+                SendReadersInfo(context);
+                LaunchController(args);
+                /*
                 Task.Run(() => LaunchController(args));
 
                 while (true)
@@ -116,7 +118,7 @@ namespace TachoClient
                         }
                     }
                     Thread.Sleep(30 * 1000);
-                }
+                }*/
             }
             catch (Exception ex)
             {
